a61da801d84f27f9910c486a2fc5968b2643a9d6
TinaHeiligers <tina.heiligers@gmail.com>
2020-08-05 14:57:01 -0700

    Adds start script to package.json

--- runConvertImperialToJsonl.js
+++ runConvertImperialToJsonl.js
@@ -1,6 +1,3 @@
-// -----------------------------------------
-// converting imperial raw to jsonl: THIS THROWS AN ERROR but it does work
-// -----------------------------------------
 const fs = require('file-system');
 const path = require('path')
 const ConvertImperialToJsonl = require('./ConvertImperialToJsonl');
