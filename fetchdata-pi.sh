c1f78cc6d7b9d157fa59cd10fa21359de88ed9e9
TinaHeiligers <tina.heiligers@gmail.com>
2020-09-26 15:44:22 -0700

    commits changes made 2 months ago

--- fetchdata-pi.sh
+++ fetchdata-pi.sh
@@ -3,4 +3,5 @@ source $HOME/.bashrc
 cd /home/pi/Projects/ambient-weather-heiligers
 echo $PWD
 echo "======================"
+echo date
 /home/pi/.config/nvm/versions/node/v14.4.0/bin/node runFetchRawData.js && /home/pi/.config/nvm/versions/node/v14.4.0/bin/node runConvertImperialToJsonl.js
