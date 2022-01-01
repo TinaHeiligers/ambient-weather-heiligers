32f186f1429f4fc4cbfcad4e8e280bd57703ede9
TinaHeiligers <tina.heiligers@gmail.com>
2021-12-31 17:29:06 -0700

    Resets back to basic working config

--- src/dataIndexers/esClient.js
+++ src/dataIndexers/esClient.js
@@ -1,15 +1,24 @@
 const { Client } = require('@elastic/elasticsearch');
+const assert = require('assert');
+const config = {
+  cloud_id: process.env.STAGING_CLOUD_ID,
+  username: process.env.STAGING_ES_USERNAME,
+  password: process.env.STAGING_ES_PASSWORD,
+}
 // this will probssbly need to be a class with methods
 // class because we want to inject the auth creds from an .env file
 // methods will include the searching for the latest indexed doc and bulk index along with others probably.
 // for now, just test that the client actually works.
+assert.ok(config.cloud_id, 'Cloud id needs to be configured');
+assert.ok(config.username, 'Cloud es cluster username needs to be configured');
+assert.ok(config.password, 'Cloud es cluster password needs to be configured');
 const client = new Client({
   cloud: {
-    id: 'staging-weather-tracker:dXMtY2VudHJhbDEuZ2NwLmNsb3VkLmVzLmlvJDVjZTFmMTVlNDdjMDQ3MzBiNjgwZjJlYTVjNDY2ODdjJDUzY2NlMWUzMTBkZTRlMzY4YmNhMGM1MDQxZmUxZjVi',
+    id: config.cloud_id,
   },
   auth: {
-    username: 'elastic',
-    password: 'B4S4T7nsWoi4bEAUtrmAhaOQ'
+    username: config.username,
+    password: config.password
   }
 });
 
