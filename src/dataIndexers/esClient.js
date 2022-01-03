0afe2034f5800dd85d74446d9b47adb0f25d7fcd
TinaHeiligers <tina.heiligers@gmail.com>
2022-01-02 18:01:13 -0700

    Adding custom client methods

--- src/dataIndexers/esClient.js
+++ src/dataIndexers/esClient.js
@@ -1,25 +1,34 @@
 const { Client } = require('@elastic/elasticsearch');
 const assert = require('assert');
-const config = {
-  cloud_id: process.env.STAGING_CLOUD_ID,
-  username: process.env.STAGING_ES_USERNAME,
-  password: process.env.STAGING_ES_PASSWORD,
-}
-// this will probssbly need to be a class with methods
-// class because we want to inject the auth creds from an .env file
-// methods will include the searching for the latest indexed doc and bulk index along with others probably.
-// for now, just test that the client actually works.
-assert.ok(config.cloud_id, 'Cloud id needs to be configured');
-assert.ok(config.username, 'Cloud es cluster username needs to be configured');
-assert.ok(config.password, 'Cloud es cluster password needs to be configured');
-const client = new Client({
-  cloud: {
-    id: config.cloud_id,
-  },
-  auth: {
-    username: config.username,
-    password: config.password
+
+function initEsClient() {
+  const envConfig = {
+    cloud_id: process.env.STAGING_CLOUD_ID,
+    username: process.env.STAGING_ES_USERNAME,
+    password: process.env.STAGING_ES_PASSWORD,
   }
-});
+
+  let missingEnvEntries = [];
+  if (!envConfig.cloud_id) missingEnvEntries.push('cloud_id')
+  if (!envConfig.username) missingEnvEntries.push('username')
+  if (!envConfig.password) missingEnvEntries.push('password');
+
+  assert.ok(envConfig.cloud_id, 'Cloud id needs to be configured');
+  assert.ok(envConfig.username, 'Cloud es cluster username needs to be configured');
+  assert.ok(envConfig.password, 'Cloud es cluster password needs to be configured');
+
+  const client = new Client({
+    cloud: {
+      id: envConfig.cloud_id,
+    },
+    auth: {
+      username: envConfig.username,
+      password: envConfig.password
+    }
+  });
+  return client;
+}
+
+const client = initEsClient()
 
 module.exports = client;
