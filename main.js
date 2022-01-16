6bd1da2bee9a49b53ce70cd28498d35023abadd8
TinaHeiligers <tina.heiligers@gmail.com>
2022-01-16 16:56:02 -0700

    Going overly complex, stashing approach for now

--- main.js
+++ main.js
@@ -75,6 +75,7 @@ async function main() {
     2: 'convertToJsonl',
     3: 'getRecentIndexedDocs',
     4: 'checkNewDataAgainstLastIndexedDoc',
+    5: 'getExistingDataFromFile'
   }
   const stepsStates = {
     fatalError: false,
@@ -83,6 +84,7 @@ async function main() {
     newDataSkipped: false,
     clusterReady: false,
     dataConvertedToJsonl: false,
+    backfillDataFromFile: false,
 
   }
   mainLogger.logInfo('starting main function', new Date());
@@ -124,22 +126,18 @@ async function main() {
   console.log()
   // this can happen regardless? yes
   const initializeResult = await dataIndexer.initialize(); // { lastIndexedImperialDoc, lastIndexedMetricDoc }
-  console.log("WHAT THE FUCK IS THIS: initializeResult", !!initializeResult === true)
-  console.log("WHAT THE FUCK IS THIS OTHER THING: Object.keys(initializeResult) === ['latestImperialDoc', 'latestMetricDoc']", Object.keys(initializeResult))
   if (!!initializeResult === true && Object.keys(initializeResult).includes('latestImperialDoc', 'latestMetricDoc')) {
     stepsStates.clusterReady = true;
-    console.log('STEPS STATE CLUSTER READY', stepsStates.clusterReady)
+    // console.log('stepsStates', stepsStates)
+    // console.log()
     lastIndexedImperialDataDate = initializeResult.latestImperialDoc[0]._source.dateutc;
-    console.log()
-    // console.log('initializeResult.latestImperialDoc[0]', initializeResult.latestImperialDoc[0])
-    console.log('initializeResult.latestImperialDoc[0]._source', initializeResult.latestImperialDoc[0]._source)
-    // console.log('initializeResult.latestImperialDoc[0]._source.dateutc', initializeResult.latestImperialDoc[0]._source.dateutc)
+    // console.log()
     // console.log('lastIndexedImperialDataDate', lastIndexedImperialDataDate)
-    console.log()
+    // console.log()
     lastIndexedMetricDataDate = initializeResult.latestMetricDoc[0]._source.dateutc;
-    console.log()
-    console.log('lastIndexedMetricDataDate', lastIndexedMetricDataDate)
-    console.log()
+    // console.log()
+    // console.log('lastIndexedMetricDataDate', lastIndexedMetricDataDate)
+    // console.log()
     // move on to indexing stuff
   }
   // check dates for what we have now compared with what's in the cluster.
@@ -158,16 +156,17 @@ async function main() {
     const dateArrayNeeded = datesForNewData?.map((fromToObj => fromToObj.to))
     if ((minDateFromDateObjects(dateArrayNeeded) - lastIndexedImperialDataDate) > 0) {
       indexImperialDocsNeeded = true;
-      mainLogger.logInfo('indexImperialDocsNeeded', indexImperialDocsNeeded);
-      console.log()
+      // mainLogger.logInfo('indexImperialDocsNeeded', indexImperialDocsNeeded);
+      // console.log()
     }
     if ((minDateFromDateObjects(dateArrayNeeded) - lastIndexedMetricDataDate) > 0) {
       indexMetricDocsNeeded = true;
-      mainLogger.logInfo('indexMetricDocsNeeded', indexMetricDocsNeeded)
-      console.log()
+      // mainLogger.logInfo('indexMetricDocsNeeded', indexMetricDocsNeeded)
+      // console.log()
     }
   } // scenario 1 or 2:
-  if (imperialDataJSONLFileNames?.length > 0) {
+  if (imperialDataJSONLFileNames.length > 0) {
+    // console.log('imperialDataJSONLFileNames.length', imperialDataJSONLFileNames.length)
     stepsStates.dataConvertedToJsonl = true;
 
     const datesFromFileNames = [
@@ -178,7 +177,8 @@ async function main() {
       indexImperialDocsFromFiles = true;
     }
   }
-  if (metricDataJSONLFileNames?.length > 0) {
+  if (metricDataJSONLFileNames.length > 0) {
+    // console.log('metricDataJSONLFileNames.length', metricDataJSONLFileNames.length)
     stepsStates.dataConvertedToJsonl = true;
     const datesFromFileNames = [
       ...metricDataJSONLFileNames.map(name => name.split('_')),
@@ -193,36 +193,75 @@ async function main() {
 
   //read all data from the imperialDataJSONLFileNames array
   // this is a prepareDataForBulkIndex method
-  if (indexImperialDocsNeeded === true || indexImperialDocsFromFiles === true) {
+  if (indexImperialDocsFromFiles === true) {
+    console.log('1. IMPERIAL HELLO?')
     const imperialDataReadyForBulkCall = prepareDataForBulkIndexing(imperialDataJSONLFileNames, 'imperial');
-    console.log('imperialDataReadyForBulkCall', imperialDataReadyForBulkCall)
-    console.log()
+    // console.log('imperialDataReadyForBulkCall', imperialDataReadyForBulkCall)
+    // console.log();
   }
-  if (indexMetricDocsNeeded === true || indexMetricDocsFromFiles === true) {
+  if (indexMetricDocsFromFiles === true) {
+    console.log('1. METRIC HELLO?')
     const metricDataReadyForBulkCall = prepareDataForBulkIndexing(metricDataJSONLFileNames, 'metric');
-    console.log('metricDataReadyForBulkCall', metricDataReadyForBulkCall)
+    // console.log('metricDataReadyForBulkCall', metricDataReadyForBulkCall)
+    // console.log();
+  }
+  if (imperialDataJSONLFileNames.length === 0) {
+    stage = step[5]
+    // mainLogger.logInfo('no new files, reading exisiting imperial data from file')
+    // console.log();
+    const imperialDataReadyForBulkCall = prepareDataForBulkIndexing(imperialDataJSONLFileNames, 'imperial');
+    // console.log('imperialDataReadyForBulkCall', imperialDataReadyForBulkCall)
+    // console.log();
+  }
+  if (metricDataJSONLFileNames.length === 0) {
+    stage = step[5]
+    // mainLogger.logInfo('no new files, reading exisiting imperial data from file')
+    // console.log();
+    const imperialDataReadyForBulkCall = prepareDataForBulkIndexing(imperialDataJSONLFileNames, 'imperial');
+    // console.log('imperialDataReadyForBulkCall', imperialDataReadyForBulkCall)
+    // console.log();
+  }
+  if (!stepsStates.fatalError === true) {
+    return 'DONE'
+  } else {
+    mainLogger.logError(stepsStates)
+    console.log()
+    mainLogger.logError(stage)
     console.log()
   }
-  return 'no new data to index'
 };
 
 function prepareDataForBulkIndexing(fileNamesArray, dataType) {
+  console.log('2. HELLO?')
+  mainLogger.logInfo('[prepareDataForBulkIndexing] [fileNamesArray]:', fileNamesArray)
+  console.log()
+  mainLogger.logInfo('[prepareDataForBulkIndexing] [dataType]:', dataType)
   let preparedData = [];
-  const fullPathToFilesToRead = `/data/ambient-weather-heiligers-${dataType}-jsonl`;
-  const fullFilePaths = fileNamesArray.map(filename => `${fullPathToFilesToRead}/${filename}`);
-  return fullFilePaths.map(fullPath => {
-    if (Object.keys(fullPath).length === 0) return true;
-    const readJsonlData = JSON.parse(fs.readFileSync(fullPath));
-    const dataWithIndexAdded = readJsonlData.flatMap(doc => [{ index: { _index: `ambient_weather_heiligers_${dataType}*` } }, doc]);
-    console.log('dataWithIndexAdded', dataWithIndexAdded)
-    preparedData.push(dataWithIndexAdded);
-    console.log('preparedData', preparedData)
-    return preparedData;
-    // read the data and add the extra stuff we need for bulkIndexing.
-    // convert it to the shape we expect to pass into bulkIndex,
-    // example doc is:
-    // {"date":"2022-01-09T00:55:00.000Z","dateutc":1641689700000,"loc":"ambient-prod-1","last_rain":"2022-01-01T10:43:00.000Z","uv":0,"wind_dir":320,"humidity":56,"humidity_inside":39,"barometer_abs_bar":9718.259152,"barometer_rel_bar":10194.385446,"temp_inside_c":21.778,"temp_outside_c":14.5,"battery_condition":"good","windspeed_km_per_hr":0,"windgust_km_per_hr":0,"max_daily_gust_km_per_hr":11.104,"hourly_rain_mm":0,"event_rain_mm":0,"daily_rain_mm":0,"weekly_rain_mm":0,"monthly_rain_mm":10,"total_rain_mm":305,"solar_radiation_W_per_sq_m":0,"feels_like_outside_c":14.5,"dewpoint_c":5.828,"feelslike_inside_c":21.056,"dewpoint_inside_c":7.222}
-  });
+  const fullPathToFilesToRead = `data/ambient-weather-heiligers-${dataType}-jsonl`;
+  if (fileNamesArray.length > 0) {
+    const fullFilePaths = fileNamesArray.map(filename => `${fullPathToFilesToRead}/${filename}.jsonl`);
+    return fullFilePaths.map(fullPath => {
+      if (Object.keys(fullPath).length === 0) return true;
+      const readJsonlData = JSON.parse(fs.readFileSync(fullPath));
+      const dataWithIndexAdded = readJsonlData.flatMap(doc => [{ index: { _index: `ambient_weather_heiligers_${dataType}*` } }, doc]);
+      console.log('!!!!!!!!!!!!!!!dataWithIndexAdded', dataWithIndexAdded)
+      preparedData.push(dataWithIndexAdded);
+      console.log('???????????????preparedData', preparedData)
+      return preparedData;
+      // read the data and add the extra stuff we need for bulkIndexing.
+      // convert it to the shape we expect to pass into bulkIndex,
+      // example doc is:
+      // {"date":"2022-01-09T00:55:00.000Z","dateutc":1641689700000,"loc":"ambient-prod-1","last_rain":"2022-01-01T10:43:00.000Z","uv":0,"wind_dir":320,"humidity":56,"humidity_inside":39,"barometer_abs_bar":9718.259152,"barometer_rel_bar":10194.385446,"temp_inside_c":21.778,"temp_outside_c":14.5,"battery_condition":"good","windspeed_km_per_hr":0,"windgust_km_per_hr":0,"max_daily_gust_km_per_hr":11.104,"hourly_rain_mm":0,"event_rain_mm":0,"daily_rain_mm":0,"weekly_rain_mm":0,"monthly_rain_mm":10,"total_rain_mm":305,"solar_radiation_W_per_sq_m":0,"feels_like_outside_c":14.5,"dewpoint_c":5.828,"feelslike_inside_c":21.056,"dewpoint_inside_c":7.222}
+    });
+  } else {
+    // read everything:
+    const files = fs.readdirSync(fullPathToFilesToRead);
+    let filesArray = [];// an array of filenames without the extension type: string[] | []
+    filesArray = files.map((file) => (`${file}`.split(".")[0])).filter((fileName => fileName.length > 0));
+    console.log('allfiles', filesArray)
+    fileNamesArray = filesArray;
+  }
+
 
 }
 
