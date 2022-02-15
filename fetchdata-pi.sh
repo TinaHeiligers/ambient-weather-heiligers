5c4f6cde17079086f6084e694ed627c8d8b0d3c6
TinaHeiligers <tina.heiligers@gmail.com>
2022-02-14 18:13:00 -0700

    update fetchdata-pi shell script

--- fetchdata-pi.sh
+++ fetchdata-pi.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-source $HOME/.bashrc
+source /home/pi/.bashrc
 cd /home/pi/Projects/ambient-weather-heiligers
 echo $PWD
 echo "======================"
 echo date
-/home/pi/.config/nvm/versions/node/v14.4.0/bin/node runFetchRawData.js && /home/pi/.config/nvm/versions/node/v14.4.0/bin/node runConvertImperialToJsonl.js && /home/pi/.config/nvm/versions/node/v14.4.0/bin/node runConvertImperialToMetric.js
+/home/pi/.config/nvm/versions/node/v16.13.1/bin/node runFetchRawData.js && /home/pi/.config/nvm/versions/node/v16.13.1/bin/node runConvertImperialToJsonl.js && /home/pi/.config/nvm/versions/node/v16.13.1/bin/node runConvertImperialToMetric.js
